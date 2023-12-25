08e4aaeedff10e87c7dacf42293d9b0300345405
Agung iskandar yuda <roku1929@gmail.com>
2023-12-25 23:04:24 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Example',
+      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
